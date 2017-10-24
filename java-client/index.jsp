84b62286e82ad94b6e53c3fde215827ca386c9f4
Aryan Agrawal <aryanagrawal@email.arizona.edu>
2017-10-24 16:11:39 -0700

    fixed connectivity issues

---
+++ java-client/index.jsp
@@ -0,0 +1,5 @@
+<jsp:useBean id="obj" class="java.util.ArrayList"/> 
+
+<jsp:useBean id="application" class="src.main.java.App"/>
+
+<%out.print("Running Structure");%>
---
+++ java-client/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import com.maven.test.App;
+
+public class Test {
+	
+	public static void main(String[] args){
+		App app = new App();
+		
+		System.out.println("App connected");
+	}
+}
