de812eb71d2dd375ec7f7b78a55dcdf162ad0002
Aryan Agrawal <aryanagrawal@email.arizona.edu>
2017-10-30 15:36:45 -0700

    alternative login init

--- WeekendVacationV1/src/main/java/request/UserSearch.java
+++ WeekendVacationV1/src/main/java/request/UserSearch.java
@@ -1,5 +1,43 @@
+
 package request;
 
-public class UserSearch {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import io.swagger.client.model.HotelPropertyResponse;
+
+public class UserSearch {
+	
+	private int membershipId;
+	private boolean isMember;
+	
+	// true if member
+	// false if not
+	public void setMemberShip(boolean isMember){
+		this.isMember = isMember;
+	}
+	
+	public boolean isUserAMember(){
+		return this.isMember;
+	}
+	
+	
+	ArrayList<Integer> membershipIds = new ArrayList<Integer>();
+	private int getMemberShipId() {
+		Random generator = new Random();
+		int key;
+		while (true) {
+			key = generator.nextInt(89999999) + 10000000;
+			if (!membershipIds.contains(key)) {
+				membershipIds.add(key);
+				return key;
+			}
+		}
+	}
+	
+	public int setMemberShipId(){
+		this.membershipId = getMemberShipId();
+		return membershipId;
+	}
 }
